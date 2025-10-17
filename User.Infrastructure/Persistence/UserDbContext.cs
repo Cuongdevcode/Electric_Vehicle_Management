4d675d7ab2cd238affbb830c3ddef88ee6e39186
Cuongdevcode <kennypham19042003@gmail.com>
2025-10-17 19:44:22 +0700

    Update structure Microservice

---
+++ User.Infrastructure/Persistence/UserDbContext.cs
@@ -0,0 +1,26 @@
+﻿using User.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace User.Infrastructure.Persistence
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User.Domain.Entities.User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Role>()
+                .HasMany(r => r.Users)
+                .WithOne(u => u.Role)
+                .HasForeignKey(u => u.RoleId);
+        }
+    }
+}
